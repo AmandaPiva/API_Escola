cdfb43c4ebebaa0115026f32fe74386c9406c965
amanda <amanda.piva01@fatec.sp.gov.br>
2023-04-14 14:55:53 -0300

    Adicionando métodos à tabela aluno

--- src/routes/alunos.js
+++ src/routes/alunos.js
@@ -1,12 +1,10 @@
 //API REST dos alunos
 import express from 'express'
-import sql from 'mssql'
+import sql, {pool} from 'mssql'
 import { sqlConfig } from '../sql/config'
 import { defaultConfiguration } from 'express/lib/application'
 
 const router = express.Router()
-
-export default router
 
 /**
  * listando os alunos
@@ -27,3 +25,60 @@ router.get('/', (req, res) => {
         console.error(`Erro ao conectar ${err.message}`)
     }
 })
+
+/**
+ * Inserindo um novo aluno
+ */
+
+router.post('/', (req, res) => {
+    const {ra, nome, cpf, dataNasc} = 
+        req.body
+    return pool.request()
+        .input('RA', sql.Int, ra)
+        .input('NOME', sql.VarChar(50), nome)
+        .input('CPF', sql.Char(20), cpf)
+        .input('DATANASCI', sql.Date, dataNasc)
+        .execute('SP_I')
+}).then(dados => {
+    res.status(200).json(dados.output)
+
+}).catch(err => {
+    res.status(400).json(err.message)
+})
+
+/**
+ * Altera um veículo existente
+ */
+
+router.put("/", (req,res) =>{
+    const {ra, nome, cpf, dataNasc} = req.body
+    return pool.request()
+        .input('RA', sql.Int, ra)
+        .input('NOME', sql.VarChar(50), nome)
+        .input('CPF', sql.Char(20), cpf)
+        .input('DATANASCI', sql.Date, dataNasc)
+        .execute('SP_I')        
+}).then(dados => {
+    res.status(200).json(`Aluno ${ra} alterado com sucesso`);
+}).catch(err => {
+    res.status(400).json(err.message)
+})
+
+/**
+ * Deletando um aluno
+ */
+
+router.delete(':/ra', (req, res) => {
+    sql.connect(sqlConfig).then(pool => {
+        const ra = req.params.ra
+        return pool.request()
+        .input('RA', sql.Int, ra)
+        .execute('SP_D_ESC_ALUNO')
+    }).then(dados => {
+        res.status(200).json(`Aluno ${ra} removido com sucesso`)
+    }).catch(err => {
+        res.status(400).json(err.message0)
+    })
+})
+
+export default router;
