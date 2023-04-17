84bd47a3abd17ca9aeacdd656b39d0130efb12bd
amanda <amanda.piva01@fatec.sp.gov.br>
2023-04-17 10:08:01 -0300

    Mais manutenções

--- src/routes/alunos.js
+++ src/routes/alunos.js
@@ -1,7 +1,7 @@
 //API REST dos alunos
 import express from 'express'
-import sql, {pool} from 'mssql'
-import { sqlConfig } from '../sql/config'
+import sql from 'mssql'
+import { sqlConfig } from '../sql/config.js'
 
 
 const router = express.Router()
@@ -11,17 +11,17 @@ const router = express.Router()
  */
 
 router.get('/', (req, res) => {
-    try{
-        sql.connect(sqlConfig).then(pool =>{
-            return pool. request()
-            .execute('SP_S_ALL_ALUNO')
+    try {
+        sql.connect(sqlConfig).then(pool => {
+            return pool.request()
+                .execute('SP_S_ALL_ALUNO')
         }).then(dados => {
             res.status(200).json(dados.recordset)
         }).catch(err => {
             res.status(400).json(err)
         })
     }
-    catch(err){
+    catch (err) {
         console.error(`Erro ao conectar ${err.message}`)
     }
 })
@@ -31,37 +31,41 @@ router.get('/', (req, res) => {
  */
 
 router.post('/', (req, res) => {
-    const {ra, nome, cpf, dataNasc} = 
-        req.body
-    return pool.request()
-        .input('RA', sql.Int, ra)
-        .input('NOME', sql.VarChar(50), nome)
-        .input('CPF', sql.Char(20), cpf)
-        .input('DATANASCI', sql.Date, dataNasc)
-        .execute('SP_I_ESC_ALUNO')
-}).then(dados => {
-    res.status(200).json(dados.output)
+    sql.connect(sqlConfig).then(pool => {
 
-}).catch(err => {
-    res.status(400).json(err.message)
+        const { ra, nome, cpf, dataNasc } =
+            req.body
+            return pool.request()
+            .input('RA', sql.Int, ra)
+            .input('NOME', sql.VarChar(50), nome)
+            .input('CPF', sql.Char(20), cpf)
+            .input('DATANASCI', sql.Date, dataNasc)
+            .execute('SP_I_ESC_ALUNO')
+    }).then(dados => {
+        res.status(200).json(dados.output)
+
+    }).catch(err => {
+        res.status(400).json(err.message)
+    })
 })
-
 /**
  * Altera um veículo existente
  */
 
-router.put("/", (req,res) =>{
-    const {ra, nome, cpf, dataNasc} = req.body
-    return pool.request()
-        .input('RA', sql.Int, ra)
-        .input('NOME', sql.VarChar(50), nome)
-        .input('CPF', sql.Char(20), cpf)
-        .input('DATANASCI', sql.Date, dataNasc)
-        .execute('SP_UP_ESC_ALUNO')        
-}).then(dados => {
-    res.status(200).json(`Aluno ${ra} alterado com sucesso`);
-}).catch(err => {
-    res.status(400).json(err.message)
+router.put("/", (req, res) => {
+    sql.connect(sqlConfig).then(pool => {
+        const { ra, nome, cpf, dataNasc } = req.body
+        return pool.request()
+            .input('RA', sql.Int, ra)
+            .input('NOME', sql.VarChar(50), nome)
+            .input('CPF', sql.Char(20), cpf)
+            .input('DATANASCI', sql.Date, dataNasc)
+            .execute('SP_UP_ESC_ALUNO')
+    }).then(dados => {
+        res.status(200).json(`Aluno ${ra} alterado com sucesso`);
+    }).catch(err => {
+        res.status(400).json(err.message)
+    })
 })
 
 /**
@@ -72,8 +76,8 @@ router.delete(':/ra', (req, res) => {
     sql.connect(sqlConfig).then(pool => {
         const ra = req.params.ra
         return pool.request()
-        .input('RA', sql.Int, ra)
-        .execute('SP_D_ESC_ALUNO')
+            .input('RA', sql.Int, ra)
+            .execute('SP_D_ESC_ALUNO')
     }).then(dados => {
         res.status(200).json(`Aluno ${ra} removido com sucesso`)
     }).catch(err => {
--- src/routes/professores.js
+++ src/routes/professores.js
@@ -1,8 +1,7 @@
 //API REST dos alunos
 import express from 'express'
-import sql, {pool} from 'mssql'
-import { sqlConfig } from '../sql/config'
-import { defaultConfiguration } from 'express/lib/application'
+import sql from 'mssql'
+import { sqlConfig } from '../sql/config.js'
 
 const router = express.Router()
 
@@ -11,17 +10,17 @@ const router = express.Router()
  */
 
 router.get('/', (req, res) => {
-    try{
-        sql.connect(sqlConfig).then(pool =>{
-            return pool. request()
-            .execute('SP_S_ALL_PROFESSOR')
+    try {
+        sql.connect(sqlConfig).then(pool => {
+            return pool.request()
+                .execute('SP_S_ALL_PROFESSOR')
         }).then(dados => {
             res.status(200).json(dados.recordset)
         }).catch(err => {
             res.status(400).json(err)
         })
     }
-    catch(err){
+    catch (err) {
         console.error(`Erro ao conectar ${err.message}`)
     }
 })
@@ -31,39 +30,43 @@ router.get('/', (req, res) => {
  */
 
 router.post('/', (req, res) => {
-    const {rf, nome, cpf, dataNasc ,especialidade} = 
-        req.body
-    return pool.request()
-        .input('RF', sql.Int, rf)
-        .input('NOME', sql.VarChar(50), nome)
-        .input('CPF', sql.Char(20), cpf)
-        .input('DATANASCI', sql.Date, dataNasc)
-        .input('ESPECIALIDADE', sql.VarChar(50), especialidade)
-        .execute('SP_I_ESC_PROFESSOR')
-}).then(dados => {
-    res.status(200).json(dados.output)
+    sql.connect(sqlConfig).then(pool => {
+        const { rf, nome, cpf, dataNasc, especialidade } =
+            req.body
+        return pool.request()
+            .input('RF', sql.Int, rf)
+            .input('NOME', sql.VarChar(50), nome)
+            .input('CPF', sql.Char(20), cpf)
+            .input('DATANASCI', sql.Date, dataNasc)
+            .input('ESPECIALIDADE', sql.VarChar(50), especialidade)
+            .execute('SP_I_ESC_PROFESSOR')
+    }).then(dados => {
+        res.status(200).json(dados.output)
 
-}).catch(err => {
-    res.status(400).json(err.message)
+    }).catch(err => {
+        res.status(400).json(err.message)
+    })
 })
 
 /**
  * Altera um professor existente
  */
 
-router.put("/", (req,res) =>{
-    const {rf, nome, cpf, dataNasc ,especialidade} = req.body
-    return pool.request()
-    .input('RF', sql.Int, rf)
-    .input('NOME', sql.VarChar(50), nome)
-    .input('CPF', sql.Char(20), cpf)
-    .input('DATANASCI', sql.Date, dataNasc)
-    .input('ESPECIALIDADE', sql.VarChar(50), especialidade)
-        .execute('SP_UP_ESC_PROFESSOR')        
-}).then(dados => {
-    res.status(200).json(`Professor ${rf} alterado com sucesso`);
-}).catch(err => {
-    res.status(400).json(err.message)
+router.put("/", (req, res) => {
+    sql.connect(sqlConfig).then(pool => {
+        const { rf, nome, cpf, dataNasc, especialidade } = req.body
+        return pool.request()
+            .input('RF', sql.Int, rf)
+            .input('NOME', sql.VarChar(50), nome)
+            .input('CPF', sql.Char(20), cpf)
+            .input('DATANASCI', sql.Date, dataNasc)
+            .input('ESPECIALIDADE', sql.VarChar(50), especialidade)
+            .execute('SP_UP_ESC_PROFESSOR')
+    }).then(dados => {
+        res.status(200).json(`Professor ${rf} alterado com sucesso`);
+    }).catch(err => {
+        res.status(400).json(err.message)
+    })
 })
 
 /**
@@ -74,8 +77,8 @@ router.delete(':/rf', (req, res) => {
     sql.connect(sqlConfig).then(pool => {
         const ra = req.params.ra
         return pool.request()
-        .input('RF', sql.Int, rf)
-        .execute('SP_D_ESC_PROFESSOR')
+            .input('RF', sql.Int, rf)
+            .execute('SP_D_ESC_PROFESSOR')
     }).then(dados => {
         res.status(200).json(`Professor ${ra} removido com sucesso`)
     }).catch(err => {
